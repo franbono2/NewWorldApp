949d85b30664d8efffa693d2ec6c861fe48188fd
Daniel Melero Garcia <danijlg92@uma.es>
2021-12-04 17:31:53 +0100

    Obtener el asentamiento seleccionado del listView y pasarlo a otra actividad mediante singletonMap

--- app/src/main/java/com/newworld/newworldapp/CitiesActivity.java
+++ app/src/main/java/com/newworld/newworldapp/CitiesActivity.java
@@ -2,11 +2,15 @@ package com.newworld.newworldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.newworld.newworldapp.db.DbHelper;
 
@@ -17,14 +21,15 @@ import java.util.Set;
 
 public class CitiesActivity extends AppCompatActivity {
     private DbHelper dbHelper;
+    ListView listView;
+    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
 
-        ListView listView = (ListView) findViewById(R.id.listview);
-        List<String> list;
+        listView = (ListView) findViewById(R.id.listview);
 
         dbHelper = (DbHelper) SingletonMap.getInstance().get("dbh");
         /*if (dbHelper == null) {
@@ -37,5 +42,19 @@ public class CitiesActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter;
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(arrayAdapter);
+        seleccionar_Asentamiento();
+    }
+
+    private void seleccionar_Asentamiento(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String seleccionado = list.get(position);
+                Toast.makeText(getApplicationContext(),"Asentamiento seleccionado: "+seleccionado,Toast.LENGTH_LONG).show();
+                SingletonMap.getInstance().put("asentamiento",seleccionado);
+                Intent intento = new Intent(getApplicationContext(), AsentamientoActivity.class);
+                startActivity(intento);
+            }
+        });
     }
 }
