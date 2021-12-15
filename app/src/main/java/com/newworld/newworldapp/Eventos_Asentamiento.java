b58f49c9aca3c8dfb8d506b4a5a442400ba66814
franbono2 <franbono2@gmail.com>
2021-12-15 12:57:27 +0100

    Eliminadas las entities y añadidos los eventos del asentamiento

---
+++ app/src/main/java/com/newworld/newworldapp/Eventos_Asentamiento.java
@@ -0,0 +1,14 @@
+package com.newworld.newworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eventos_Asentamiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos_asentamiento);
+    }
+}
