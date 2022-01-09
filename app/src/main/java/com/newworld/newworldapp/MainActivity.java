d21739b464a1f4a4d7550922da715b44b0bfb28a
Daniel Melero Garcia <danijlg92@uma.es>
2022-01-09 17:11:32 +0100

    Personalizacion calendario

--- app/src/main/java/com/newworld/newworldapp/MainActivity.java
+++ app/src/main/java/com/newworld/newworldapp/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Window window = this.getWindow();
-        /*window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);*/
         window.setStatusBarColor(this.getResources().getColor(R.color.main));
 
         dbHelper = (DbHelper) SingletonMap.getInstance().get("dbh");
